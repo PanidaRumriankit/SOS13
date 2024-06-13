c9abca198f4fdbcb663da79d55b06ec111bac3df
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2024-06-13 18:31:32 +0700

    Finish 3 question of day1. going to have the dinner.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -13,11 +13,27 @@ const slides = [
 
 const problems = [
   { title: "print()",
-    describe: "day1",
+    describe: "Day1",
     day: 1,
     link: "https://elabsheet.org/elab/taskpads/show/9k7wrh59oh/",
     img: "",
     id: 1 
+  },
+
+  { title: "Basic Operations 1",
+    describe: "Day1",
+    day: 1,
+    link: "https://elabsheet.org/elab/taskpads/show/ovus65cebm/",
+    img: "",
+    id: 2 
+  },
+
+  { title: "Basic Operations 2",
+    describe: "Day1",
+    day: 1,
+    link: "https://elabsheet.org/elab/taskpads/show/5chqulocvs/",
+    img: "",
+    id: 3 
   }
 ];
 
@@ -25,13 +41,29 @@ const problems = [
 const for_sos14 = [
 /*Hi, My junior for_sos14 const is used for store modifiable Elab code. 
 If SOS14 is coming and you have no idea how to write Elab problems. You can use these Elab links to see my Elab codes.
-However, don't modifly my code only modifly your own code. It will affects SOS13 problems if you modify it. 
+However, don't modifly my code only modifly your own code. It will affects SOS13 problems if you modify it. (I'm not sure.) 
 Ichi will come to your house at 5 a.m. if you modifly it.*/
 
   {
     title: "print()",
     day:1,
-    link: "https://elabsheet.org/elab/taskpads/change/9k7wrh59oh/gdrpx14n2x/"
+    link: "https://elabsheet.org/elab/taskpads/change/9k7wrh59oh/gdrpx14n2x/",
+    id: 1
+  },
+
+  {
+    title: "Basic Operations 1",
+    day:1,
+    link: "https://elabsheet.org/elab/taskpads/change/ovus65cebm/8xms5dxc1y/",
+    id: 2
+  },
+
+  {
+    title: "Basic Operation 2",
+    day: 1,
+    link: "https://elabsheet.org/elab/taskpads/change/5chqulocvs/88fof3abfu/",
+    id: 3
   }
-]
+];
+
 export { navLinks, slides, problems };
