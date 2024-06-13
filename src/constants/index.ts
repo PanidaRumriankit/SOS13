5ccee57f0ae5fca56a0d3753d2b8adcd83527b62
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2024-06-13 17:15:13 +0700

    add first problem and create for_sos14 const

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -12,12 +12,26 @@ const slides = [
 ];
 
 const problems = [
-  { title: "",
+  { title: "print()",
     describe: "day1",
     day: 1,
-    link: "",
+    link: "https://elabsheet.org/elab/taskpads/show/9k7wrh59oh/",
     img: "",
-    id: 1 }
+    id: 1 
+  }
 ];
 
+
+const for_sos14 = [
+/*Hi, My junior for_sos14 const is used for store modifiable Elab code. 
+If SOS14 is coming and you have no idea how to write Elab problems. You can use these Elab links to see my Elab codes.
+However, don't modifly my code only modifly your own code. It will affects SOS13 problems if you modify it. 
+Ichi will come to your house at 5 a.m. if you modifly it.*/
+
+  {
+    title: "print()",
+    day:1,
+    link: "https://elabsheet.org/elab/taskpads/change/9k7wrh59oh/gdrpx14n2x/"
+  }
+]
 export { navLinks, slides, problems };
