c9ac58b71fa6330b7af610815765c8fee4198aa0
PanidaRumriankit <panida.rum@ku.th>
2024-07-09 19:20:53 +0700

    better contacts page

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import TextField, { TextFieldProps } from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
+import { styled } from '@mui/material/styles';
 import Navbars from './Navbars';
 import Footer from './Footer';
+import starburst from '../assets/star_burst.mp4';
 import Box from '@mui/material/Box';
 
-export default function SignUp() {
+export default function Contact() {
   const current_page = "/Contacts";
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -16,88 +17,173 @@ export default function SignUp() {
     const data = new FormData(event.currentTarget);
     console.log({
       email: data.get('email'),
-      password: data.get('password'),
     });
   };
 
+  const WhiteTextField = styled((props: TextFieldProps) => (
+    <TextField
+      {...props}
+      InputProps={{
+        ...props.InputProps,
+        sx: {
+          color: 'white',
+          '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+              borderColor: 'white',
+            },
+            '&:hover fieldset': {
+              borderColor: 'white',
+            },
+            '&.Mui-focused fieldset': {
+              borderColor: 'white',
+            },
+          },
+        },
+      }}
+      InputLabelProps={{
+        ...props.InputLabelProps,
+        sx: {
+          color: 'white',
+          '&.Mui-focused': {
+            color: 'white',
+          },
+        },
+      }}
+    />
+  ))({
+    '& .MuiInput-underline:before': {
+      borderBottomColor: 'white',
+    },
+    '& .MuiInput-underline:hover:before': {
+      borderBottomColor: 'white',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: 'white',
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: 'white',
+      },
+      '&:hover fieldset': {
+        borderColor: 'white',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: 'white',
+      },
+    },
+  });
+
   return (
-    <>
-      <Navbars page={ current_page }/>
-      <Container sx={{
+    <Box
+      id="problem"
+      sx={(theme) => ({
+        position: 'relative',
+        width: '100%',
+        minHeight: '100vh',
+        overflow: 'hidden',
+        color: 'white',
         display: 'flex',
         flexDirection: 'column',
-        pt: { xs: 4, sm: 8 },
-        pb: { xs: 4, sm: 8 },
-        }}>
+      })}
+      className="pt-16"
+      >
+        <video
+        autoPlay
+        muted
+        loop
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+          zIndex: -1
+        }}
+      >
+        <source src={starburst} type="video/mp4" />
+      </video>
+      <Navbars page={ current_page }/>
         <Box
           sx={{
-            marginTop: 8,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
+            justifyContent: 'center',
+            flexGrow: 1,
+            width: '100%',
           }}
         >
-          <Typography component="h1" variant="h5">
-            Contact us via email
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
+          <Box
+            sx={{
+              backgroundColor: 'rgba(255, 255, 255, 0.2)',
+              padding: 3,
+              borderRadius: 2,
+              width: '100%',
+              maxWidth: 600,
+            }}
+          >
+            <Typography component="h1" variant="h5" sx={{ color: 'primary.main', fontWeight: 'bold' }}>
+              Contact us via email
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <WhiteTextField
+                    autoComplete="given-name"
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <WhiteTextField
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    name="lastName"
+                    autoComplete="family-name"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <WhiteTextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <WhiteTextField
+                    required
+                    fullWidth
+                    name="message"
+                    label="Message"
+                    type="message"
+                    id="message"
+                    multiline
+                    rows={4}
+                  />
+                </Grid>
               </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="message"
-                  label="Message"
-                  type="message"
-                  id="message"
-                  multiline
-                  rows={4}
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Submit
-            </Button>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Submit
+              </Button>
+            </Box>
           </Box>
         </Box>
-      </Container>
       <Footer />
-    </>
+    </Box>
   );
 }
